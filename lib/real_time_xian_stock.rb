ccd2b19054eb7b5fdb34fb8d4fa2c70861b5f913
Jack Wang <wfenhan@gmail.com>
2017-09-07 08:15:50 -0400

    Change to Yahoo

--- lib/real_time_xian_stock.rb
+++ lib/real_time_xian_stock.rb
@@ -59,6 +59,7 @@ class RealTimeXianStock
       @stock_abb_array = @client[:stocks].find({}).map{|e| e["stock_name"]}
     end
     @stock_abb_array.each_slice(100) {|su_arr|
+    # @stock_abb_array.each {|su_arr|
       # http://wern-ancheta.com/blog/2015/04/05/getting-started-with-the-yahoo-finance-api/
       # s: Symbol, a: Ask, b: Bid, b2: Ask (Realtime), b3: Bid (Realtime), k: 52 Week High, j: 52 week Low, 
       # j6: Percent Change From 52 week Low, k5: Percent Change From 52 week High, v: Volume, j1: Market Capitalization
@@ -73,16 +74,18 @@ class RealTimeXianStock
       # d2 – trade date
       # t1 – last trade time
       # c8 – after hours change
-      sub_url  = su_arr.join(",")
-      full_url = "http://finance.google.com/finance/info?client=ig&q=NASDAQ%3A#{sub_url}"
-      get_stockinfo_from_google(full_url)
+      # sub_url  = su_arr.join(",")
+      # full_url = "http://finance.google.com/finance/info?client=ig&q=NASDAQ%3A#{sub_url}"
+      # full_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{su_arr}&interval=1min&apikey=4IW1LB9F2Y5ISREC"
+      # get_stockinfo_from_google(full_url)
+      # get_stockinfo_from_alphavantage(full_url)
+      # sleep 0.01
 
 
       # if ( Time.now.min / 10 ).even?
-      #   sub_url  = su_arr.join("+")
-      #   full_url = "http://download.finance.yahoo.com/d/quotes.csv?s=#{sub_url}&f=sc1cc6k2p2d1d2t1c8"
-      #   get_stockinfo_from_yahoo(full_url)
-      # else
+        sub_url  = su_arr.join("+")
+        full_url = "http://download.finance.yahoo.com/d/quotes.csv?s=#{sub_url}&f=sc1cc6k2p2d1d2t1c8"
+        get_stockinfo_from_yahoo(full_url)
       # end
     }
   end
@@ -208,6 +211,84 @@ class RealTimeXianStock
       @count += 1
 
     }
+  end
+
+  def get_stockinfo_from_alphavantage(url)
+    begin
+      # respon = open(url)
+      respon = URI.parse(url).read
+      # raise "connecton" unless respon.status.first == "200"
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+      # binding.pry
+      sleep 1
+      retry            
+    end
+    result_arr = JSON.parse(respon)
+    begin
+    stock_name    = result_arr["Meta Data"]["2. Symbol"]
+      
+    rescue Exception => e
+     return false      
+    end
+    changed_value = result_arr["Time Series (1min)"].first.last["4. close"].to_f
+    open_price    = result_arr["Time Series (1min)"].first.last["1. open"].to_f
+    percent_stri  = ( changed_value - open_price ) / open_price
+    # unless result_arr["ec"].nil?
+    #   pre_changeval = result_arr["ec"].to_s.gsub(/\+|\-|\%/, '').to_f
+    #   pre_market_ch = result_arr["ecp"]
+    #   pre_percent_num = pre_market_ch.gsub(/\+|\-|\%/, '').to_f
+    # end
+    iS_increased  = false
+    # iS_pre_increase = false
+    iS_xiangu     = false
+    if percent_stri > 0
+      iS_increased = true
+    end
+    # if result_arr["ec"] =~ /\+/
+    #   iS_pre_increase = true
+    # end
+    percent_num = percent_stri * 100
+    # binding.pry if stock_name == "ESES"
+    trade_date = result_arr["Meta Data"]["3. Last Refreshed"] #%d\%m%m\xxxx
+    puts percent_num, percent_stri, stock_name
+    if ( iS_increased and percent_num > 20 )
+      puts "#{@count}"
+      puts "Name: #{stock_name},  increase?: #{iS_increased}, percent: #{percent_num}, last_time: #{trade_date}"
+      puts ""
+    end
+    minutes_run = ( Time.now.to_i - Time.parse("#{Time.now.strftime('%Y-%m-%d')} 09:30:00").to_i) / 60
+    collection = @client[:stocks]
+    three_minute_rate = percent_num / minutes_run * 3
+
+# ============== how to define a xian gu ======================
+    if ( percent_num > 30 and iS_increased )
+      iS_xiangu = true
+    elsif ( three_minute_rate ) > 9 and iS_increased
+      iS_xiangu = true
+    end
+# ================================================================
+
+
+    if collection.find( { stock_name:  stock_name } ).first.nil?
+      result = collection.insert_one({ stock_name: stock_name, 
+                                       stock_increased: iS_increased,
+                                       percent_num: percent_num,
+                                       three_minute_rate: three_minute_rate,
+                                       #pre_market: pre_percent_num,
+                                       xian_gu: iS_xiangu
+      })
+    else
+      result = collection.update_one( { stock_name: stock_name },
+                                      { '$set': { stock_increased: iS_increased,
+                                                  percent_num: percent_num,
+                                                  three_minute_rate: three_minute_rate,
+                                                  #pre_market: pre_percent_num,
+                                                  xian_gu: iS_xiangu 
+                                    } } )
+    end
+    @count += 1
   end
 
 end
